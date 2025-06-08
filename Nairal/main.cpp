2b9f44aaca883d3cc2fc5bc84a68c2b53dcfda13
Jake Kong <82667303+JakeKrong@users.noreply.github.com>
2025-06-08 17:37:31 +0800

    Added SFML / CMake Config

--- Nairal/main.cpp
+++ Nairal/main.cpp
@@ -3,10 +3,11 @@
 #include <chrono>
 #include <thread>
 #include "GameStateManager.h"
-//#include "ExplorationState.h"
+#include<optional>
+#include <SFML/Graphics.hpp>
 
 
-#include "Nairal.h"
+#include "main.h"
 
 class Nairal
 {
@@ -63,7 +64,22 @@ private:
 
 int main()
 {
-	Nairal nairal;
-	nairal.Run();
+	//Nairal nairal;
+	//nairal.Run();
+	sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "SFML 3 + C++20");
+
+	while (window.isOpen()) 
+	{
+		while (const std::optional event = window.pollEvent())
+		{
+			if (event->is < sf::Event::Closed>())
+			{
+				window.close();
+			}
+		}
+
+		window.clear();
+		window.display();
+	}
 	return 0;
 }
